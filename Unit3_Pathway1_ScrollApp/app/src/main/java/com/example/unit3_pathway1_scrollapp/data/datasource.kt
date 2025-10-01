4d8e712ad07d06e794fdeade3efc415b6832aa36
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2025-10-01 22:50:56 +0700

    finish Practice: Build a grid

--- Unit3_Pathway1_ScrollApp/app/src/main/java/com/example/unit3_pathway1_scrollapp/data/datasource.kt
+++ Unit3_Pathway1_ScrollApp/app/src/main/java/com/example/unit3_pathway1_scrollapp/data/datasource.kt
@@ -1,7 +1,9 @@
 package com.example.unit3_pathway1_scrollapp.data
 import com.example.unit3_pathway1_scrollapp.model.Affirmation
 import com.example.unit3_pathway1_scrollapp.R
+import com.example.unit3_pathway1_scrollapp.model.Topic
 
+/*
 class datasource {
         fun loadAffirmations(): List<Affirmation> {
             return listOf<Affirmation>(
@@ -18,3 +20,22 @@ class datasource {
                 Affirmation(R.string.cat11, R.drawable.cat11))
         }
     }
+*/
+object dataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 164, R.drawable.gaming),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.tech, 118, R.drawable.tech)
+    )
+}
---
+++ Unit3_Pathway1_ScrollApp/app/src/main/java/com/example/unit3_pathway1_scrollapp/model/Topic.kt
@@ -0,0 +1,10 @@
+package com.example.unit3_pathway1_scrollapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
