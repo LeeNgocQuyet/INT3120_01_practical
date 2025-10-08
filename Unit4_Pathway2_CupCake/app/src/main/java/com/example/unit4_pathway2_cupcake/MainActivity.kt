816db3af5bc6309c421c06dcabf50a83a7161a9d
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2025-10-09 00:50:44 +0700

    Finish Navigate between screens with Compose

---
+++ Unit4_Pathway2_CupCake/app/src/main/java/com/example/unit4_pathway2_cupcake/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.unit4_pathway2_cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import com.example.unit4_pathway2_cupcake.ui.theme.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
