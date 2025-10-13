31eaaf6f75b221eca244abdb41a8e41afccc677c
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2025-10-13 22:50:48 +0700

    finish Practice: Build Sports app

---
+++ Unit4_Pathway3_TrainingSport/build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("lifecycle_version", "2.8.7")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.13.0" apply false
+    id("com.android.library") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+}
