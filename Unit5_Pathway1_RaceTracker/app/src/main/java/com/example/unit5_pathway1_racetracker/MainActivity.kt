92cc7297ddbfa67ad947f0d4302b5f6684116882
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2025-10-15 21:19:38 +0700

    finish Introduction to Coroutines in Android Studio

---
+++ Unit5_Pathway1_RaceTracker/app/src/main/java/com/example/unit5_pathway1_racetracker/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.unit5_pathway1_racetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unit5_pathway1_racetracker.ui.RaceTrackerApp
+import com.example.unit5_pathway1_racetracker.ui.theme.RaceTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            RaceTrackerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    RaceTrackerApp()
+                }
+            }
+        }
+    }
+}
